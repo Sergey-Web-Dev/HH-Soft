4e623f0eb8e50cd886ade70d6078387ac69e0cb2
Sergey Lobodin <lobodin01@mail.ru>
2023-12-12 16:09:22 +0300

    configured routers from react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import {
+  Home,
+  BimContent,
+  SupportBim,
+  Education,
+  Contacts,
+} from "./pages/index";
 
 export const App = () => {
-  return <div className="App"></div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/bim_content" element={<BimContent />} />
+      <Route path="/support_bim" element={<SupportBim />} />
+      <Route path="/education" element={<Education />} />
+      <Route path="/contacts" element={<Contacts />} />
+    </Routes>
+  );
 };
---
+++ src/pages/SupportBim.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const SupportBim = () => {
+  return <div>SupportBim</div>;
+};
